776f527387b7e3f900cc41fc7a6fec3f393e890c
Cindy-O <cindy.orbegoso@yahoo.ca>
2017-02-12 13:33:59 -0500

    removed unnecessary files and fixed index

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Testing' });
 });
 
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
 module.exports = router;
